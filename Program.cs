6901f3305ecb1cb9037789bc4f2adc9a4afaf271
Stef Dunlap <stef@kindrobot.ca>
2022-05-15 20:48:58 -0400

    Add argument to specify which camera to use

--- Program.cs
+++ Program.cs
@@ -5,18 +5,28 @@ public static class Program
 {
     public static int Main(string[] args)
     {
-        if (args.Length == 0 || string.IsNullOrEmpty(args[0])) {
+        string outputPath;
+        int cameraIndex;
+
+        if (args.Length != 2 || string.IsNullOrEmpty(args[0])) {
             Console.WriteLine("Path missing, usage example:");
-            Console.WriteLine("openphoto.exe /Users/bob/Desktop/me.png");
+            Console.WriteLine("  openphoto.exe CAMERA_INDEX OUTPUT_FILE_PATH");
+            Console.WriteLine("");
+            Console.WriteLine("Example:");
+            Console.WriteLine("  openphoto.exe 0 C:\\Users\\user\\foo.png");
+
             return 1;
+        } else {
+            cameraIndex = Int16.Parse(args[0]);
+            outputPath = args[1];
         }
-        
+
         try {
-            var capture = new VideoCapture(0);
+            var capture = new VideoCapture(cameraIndex);
             var image = new Mat();
             capture.Read(image);
             var bitmap = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(image);
-            bitmap.Save(args[0]);
+            bitmap.Save(outputPath);
             return 0;
         } catch(Exception ex) {
             Console.WriteLine(ex.Message);
